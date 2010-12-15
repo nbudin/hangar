4d6d4a3049f3d8de437b5b00872f10b3fdad2fda
Nat Budin <natbudin@gmail.com>
2010-12-15 16:48:40 -0500

    Bundler support on load

--- rb_main.rb
+++ rb_main.rb
@@ -11,10 +11,32 @@
 framework 'Cocoa'
 framework 'WebKit'
 
-# Loading all the Ruby project files.
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+# Load all the bundled gems
+require 'rubygems'
+Gem.clear_paths
+Gem.path.clear
+Dir[File.join(dir_path, "..", "Frameworks", "MacRuby.framework", 
+			  "Versions", "*", "usr", "lib", "ruby", "Gems", "*")].each do |dir|
+  Gem.path << File.expand_path(dir)
+end
+puts "Bootstraping Bundler with Gem.path #{Gem.path.inspect}"
+
+ENV['BUNDLE_GEMFILE'] = File.join(dir_path, "Gemfile")
+ENV['BUNDLE_PATH'] = File.join(dir_path, "vendor", "bundle", 
+							   Gem.ruby_engine, Gem::ConfigMap[:ruby_version])
+ENV['BUNDLE_FROZEN'] = "1"
+ENV['BUNDLE_ROOT'] = dir_path
+
+require 'bundler'
+puts "Bundler lockfile: #{Bundler.default_lockfile}"
+puts "Bundler path: #{Bundler.settings.path}"
+Bundler.setup
+puts "Gem.path after running Bundler setup: #{Gem.path.inspect}"
+
+# Loading all the Ruby project files.
 Dir.entries(dir_path).each do |path|
-  if path != File.basename(__FILE__) and path.match(/\.rbo?$/)
+  if path != File.basename(__FILE__) and path.match(/hangar_application_controller\.rbo?$/)
     require(path)
   end
 end
